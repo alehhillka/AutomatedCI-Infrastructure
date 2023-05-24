7c24eff270a2d5ba76de42d54331a05ec7e2f254
Oleh Pilipenko <oleg.pilipenko26@gmail.com>
2023-05-24 09:08:47 +0300

    Add IAM role and policies using Terraform

---
+++ iam.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "project_role" {
+  name = "project_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.project_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.project_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+  role       = aws_iam_role.project_role.name
+}
+
+
+resource "aws_iam_instance_profile" "project_profile" {
+  name = "project_profile"
+  role = aws_iam_role.project_role.name
+}
